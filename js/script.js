5c925ecfdb64cf2a9b1279de3aae17b664af1c82
Yaroslav <yaros.rus@yandex.ru>
2016-05-16 23:02:42 +0500

    js form

---
+++ js/script.js
@@ -0,0 +1,7 @@
+		var link = document.querySelector(".form_title");
+		var popup = document.querySelector(".form_params");
+
+		link.addEventListener("click" fuction("event") {
+			event.preventDefault();
+			popup.classList.add("form_params_show");
+		})
